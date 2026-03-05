b6f6494cd004dba468507650776439483f6955fa
dzarembo <dima.zarembo@gmail.com>
2026-03-06 00:34:41 +0300

    Added access only for USER and ADMIN in TaskController

--- src/main/java/com/example/demo/controllers/task/TaskController.java
+++ src/main/java/com/example/demo/controllers/task/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ import java.util.List;
 @AllArgsConstructor
 @Validated
 @SecurityRequirement(name = "bearerAuth")
+@PreAuthorize("hasAnyRole('USER','ADMIN')")
 public class TaskController {
 
     private final TaskService taskService;
